4d853e34fd09c36ab630f4b84a04acc9d980751b
leeenx <lee200822@126.com>
2019-03-29 10:24:21 +0800

    支持multiple touuch

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     index: './src/script/index'
   },
